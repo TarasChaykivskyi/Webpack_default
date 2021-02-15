9f14b75e243e88980d3a8429505a8376dc8be56b
TarasChaykivskyi <webrobs000@gmail.com>
2021-02-15 20:12:42 +0200

    add copy plugin, fix folder css and js

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
+const source_directory = 'src';
+const project_directory = 'dist';
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const filename = ext => isDev ? `${ext}/[name].bundle.${ext}` : `${ext}[name].bundle.[hash].${ext}`;
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
+    context: path.resolve(__dirname, source_directory),
+    entry: './js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, project_directory),
+        filename: filename('js')
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -21,8 +24,20 @@ module.exports = {
             template: 'index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].bundle.css'
-        })
+            filename: filename('css')
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, source_directory, "img"),
+                    to: path.resolve(__dirname, project_directory, "img"),
+                },
+                {
+                    from: path.resolve(__dirname, source_directory, "fonts"),
+                    to: path.resolve(__dirname, project_directory, "fonts"),
+                },
+            ],
+        }),
     ],
     module: {
         rules: [
